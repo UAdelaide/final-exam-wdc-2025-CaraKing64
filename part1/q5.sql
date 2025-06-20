fe2388d1f6be62016c273def3e68a19e1e7bb65e
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 04:46:39 +0000

    [automatic commit] 2025-06-20 04:46:39 UTC 203.221.20.54

--- part1/q5.sql
+++ part1/q5.sql
@@ -1,3 +1,3 @@
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'), ('bobwalker', 'bob@example.com', 'hashed456', 'walker'), ('carol123', 'carol@example.com', 'hashed789', 'owner'), ('dantheman', 'dan@example.com', 'hashed222', 'owner'), ('ellywalks', 'elly@example.com', 'hashed444', 'walker');
 INSERT INTO Dogs (owner_id, name, size) VALUES ( (SELECT user_id FROM Users WHERE username = 'alice123' ), 'Max', 'medium'), ( (SELECT user_id FROM Users WHERE username = 'carol123' ), 'Bella', 'small'), ( (SELECT user_id FROM Users WHERE username = 'carol123' ), 'Ginger', 'small'), ( (SELECT user_id FROM Users WHERE username = 'dantheman' ), 'Rex', 'medium'), ( (SELECT user_id FROM Users WHERE username = 'dantheman' ), 'Gojo', 'large');
-INSERT INTO WalkRequests
+INSERT INTO WalkRequests (dog_id, oww)
