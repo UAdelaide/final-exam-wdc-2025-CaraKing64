bb18fc037cb9d2d9b983284b42f594e556e9df8c
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 04:34:42 +0000

    [automatic commit] 2025-06-20 04:34:42 UTC 203.221.20.54

--- part1/q5.sql
+++ part1/q5.sql
@@ -1,4 +1,5 @@
 
 
 
+INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'); VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
 INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner');
