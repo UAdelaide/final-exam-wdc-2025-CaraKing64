b7d100ec5f3ab91e46e01f3e4617de887b017989
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 05:20:05 +0000

    [automatic commit] 2025-06-20 05:20:05 UTC 203.221.20.54

--- part1/app.js
+++ part1/app.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+let db;
 (async () => {
   try {
     // Connect to MySQL without specifying a database
