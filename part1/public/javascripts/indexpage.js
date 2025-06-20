33ef9ff0c56b9555518e91680b83f2d1f4084d63
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 08:31:56 +0000

    [automatic commit] 2025-06-20 08:31:56 UTC 203.221.20.54

--- part1/public/javascripts/indexpage.js
+++ part1/public/javascripts/indexpage.js
@@ -8,6 +8,8 @@ const vueinst = new Vue({
 });
 
 async function getDogImage(){
+  vueinst.showing_dog = !vueinst.showing_dog;
+  if (showing_dog)
   var response = await fetch('https://dog.ceo/api/breeds/image/random');
   vueinst.image_url = response.body.message;
   console.log(vueinst.image_url);
