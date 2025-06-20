0b5e022b97b8722bbf6be1ece71aa1374bc379e9
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 11:00:24 +0000

    [automatic commit] 2025-06-20 11:00:24 UTC 203.221.20.54

--- part2/app.js
+++ part2/app.js
@@ -7,6 +7,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
