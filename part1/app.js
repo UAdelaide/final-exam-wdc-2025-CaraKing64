448a3772405cec1c7ca50ee81d044eed7efa9ac6
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 05:22:09 +0000

    [automatic commit] 2025-06-20 05:22:09 UTC 203.221.20.54

--- part1/app.js
+++ part1/app.js
@@ -107,6 +107,7 @@ let db;
         ('Brave New World', 'Aldous Huxley')
       `);
     }
+    
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
