67bab53c260010810b9eacb4ff64ce01c9991eb8
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 07:21:09 +0000

    [automatic commit] 2025-06-20 07:21:09 UTC 203.221.20.54

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -229,7 +229,7 @@ router.get('/api/walkers/summary', async function(req, res, next){
         w.rating_sum += ratings[j].rating;
       }
     }
-    // 
+    // if there were ratings, average them
     if (w.total_ratings != 0){
       w.average_rating = w.rating_sum / w.rating_count;
     }
