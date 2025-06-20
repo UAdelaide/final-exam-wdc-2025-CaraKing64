1c4cfa21e187f27e036ce9ca5b10e617ce03ab64
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 11:47:43 +0000

    [automatic commit] 2025-06-20 11:47:43 UTC 203.221.20.54

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -40,7 +40,9 @@ var options = { root: '.' };
 router.post('/login', async (req, res) => {
   if (req.session.isLoggedIn){
     // User is already logged in
-    res.status(403).json({error: 'You are already logged in'});
+res.status(403).json({error: 'You are already logged in'});
+
+
   }
   const { username, password } = req.body;
   // Get the user with the provided username
