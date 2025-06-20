7ea2887c46a0caae4153a6d28fc316746ae2cd05
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 11:07:15 +0000

    [automatic commit] 2025-06-20 11:07:15 UTC 203.221.20.54

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -39,7 +39,8 @@ router.get('/me', (req, res) => {
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
   console.log(username, password);
-  const [rows] = db.execute('');
+  const [rows] = db.query(`SELECT * FROM`, []);
+  console.log(rows);
 
 
   /*
