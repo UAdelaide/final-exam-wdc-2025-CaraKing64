655a91ff58492a76a1c54f44d68d05493d39e0a1
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 14:05:55 +0000

    [automatic commit] 2025-06-20 14:05:55 UTC 203.221.20.54

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -34,9 +34,9 @@ router.get('/me', async (req, res) => {
     return res.status(401).json({ error: 'Not logged in' });
   }
   const [row] = await db.query(`SELECT user_id, username, email, role, created_at FROM Users WHERE Username = ?`, [req.session.username]);
-  if (row.length < 1){
+  if (row.length < 1){ // if there is not a user with this username
     res.status(500).json({error: 'No user exists in the database with your username'});
-  } else if (row.length > 1){
+  } else if (row.length > 1){ // if there is more than 1 user with this username
     res.status(500).json({error: 'More than 1 user exists in the database with your username'});
   } else {
     res.send(row[0]);
