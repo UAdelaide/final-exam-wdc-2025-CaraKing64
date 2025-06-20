bc9b9e4a22c4fe63d0976b86db277beadae0acd8
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-20 05:27:16 +0000

    [automatic commit] 2025-06-20 05:27:16 UTC 203.221.20.54

--- part1/app.js
+++ part1/app.js
@@ -113,12 +113,11 @@ let db;
     if (rows2[0].count === 0){
       await db.execute(`
         INSERT INTO Dogs (owner_id, name, size) VALUES
-        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Ginger', 'small'),
-        ((SELECT user_id FROM Users WHERE username = 'dantheman'), 'Rex', 'medium'),
-        ((SELECT user_id FROM Users WHERE username = 'dantheman'), 'Gojo', 'large');
-
+          ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+          ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+          ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Ginger', 'small'),
+          ((SELECT user_id FROM Users WHERE username = 'dantheman'), 'Rex', 'medium'),
+          ((SELECT user_id FROM Users WHERE username = 'dantheman'), 'Gojo', 'large');
       `);
     }
 
