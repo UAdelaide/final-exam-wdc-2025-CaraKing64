74a774d8b961818f0a0185989cd5e8b6b0231c99
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-21 02:22:36 +0000

    [automatic commit] 2025-06-21 02:22:36 UTC 203.221.20.54

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -85,9 +85,8 @@ router.get('/alldogs', async (req, res) => {
       res2 = await res2.json();
       var dog = rows[i];
       dog.photo = res2.message;
-      var colours = ['light-grey', 'white'];
+      var colours = ['lightgrey', 'white'];
       dog.table_colour = colours[i%2];
-      // console.log(dog);
       dogs.push(dog);
       // dogs.value.photo = res.message;
       // console.log(dogs.value);
