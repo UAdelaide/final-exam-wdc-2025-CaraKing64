b3a10731c14a4e8e5f29c41cb3063f6156fe0fbb
Isaac Carabott <a1926322@adelaide.edu.au>
2025-06-21 03:29:23 +0000

    [automatic commit] 2025-06-21 03:29:22 UTC 203.221.20.54

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -209,7 +209,7 @@ router.get('/api/walkers/summary', async function(req, res, next){
   // get all ratings for all walkers
   const [ratings] = await db.execute(`SELECT username, rating FROM (Users INNER JOIN WalkRatings ON Users.user_id = WalkRatings.walker_id)`);
   // get all completed walks (assuming that accepted walks are completed walks and nothing went wrong / no unlogged cancellations)
-  const [completedwalks] = await db.execute(`SELECT username FROM WalkApplications INNER JOIN Users ON WalkApplications.walker_id = Users.user_id WHERE status = 'accepted'`);
+  const [completedwalks] = await db.execute(`SELECT walker_id, WalkRequests.status, WalkApplications.status FROM WalkApplications INNER JOIN WalkRequests ON WalkApplications.request_id = WalkRequests.request_id WHERE WalkRequests.status = 'completed' and WalkApplications.status = 'accepted'`);
 
   console.log(completedwalks);
   // loop through all the walkers and process them
